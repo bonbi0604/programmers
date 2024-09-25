b8be65082311f3d903fd1c93b86a0f672df43499
bonbi0604 <152830888+bonbi0604@users.noreply.github.com>
2024-09-26 00:40:00 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <set>
+#include <math.h>
+using namespace std;
+
+int main() {
+
+        cin.tie(NULL);
+        cout.tie(NULL);
+        
+        int n;
+        cin >> n;
+
+        cout << int(sqrt(n));
+
+        return 0;
+}
