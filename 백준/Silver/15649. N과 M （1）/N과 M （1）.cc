7d68e2a80302eb91a8ec3e7bb81acd44e6d8c666
bonbi0604 <152830888+bonbi0604@users.noreply.github.com>
2024-09-27 19:09:10 +0900

    [Silver III] Title: N과 M (1), Time: 48 ms, Memory: 6384 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+
+int main() {
+        cin.tie(NULL);
+        cout.tie(NULL);
+        int n,  m;
+        cin >> n >> m;
+        set<vector<int>>s;
+        vector<int>v(n), zero;
+        for (int i = 0; i < n; i++) {
+	v[i] = i + 1;
+        }
+        for (int i = 0; i < m; i++) zero.push_back(1);
+
+        do {
+	vector<int>tmp;
+	for (int i = 0; i < m; i++) {
+	        tmp.push_back(v[i]);
+	}
+	s.insert(tmp);
+        } while (next_permutation(v.begin(), v.end()));
+
+        for (auto e : s) {
+	for (auto k : e) {
+	        cout << k << " ";
+	}
+	cout << "\n";
+        }
+
+        return 0;
+}
+
